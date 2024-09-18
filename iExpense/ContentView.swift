9fd43081b78baaa421112542fb12f0b90de53b8b
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-09-18 13:51:57 -0600

    iExpense: Introduction, Using @State with classes, Sharing SwiftUI state with @Observable

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -6,16 +6,26 @@
 //
 
 import SwiftUI
+import Observation
+
+
+@Observable
+class User {
+    var firstName = "Bilbo"
+    var lastName = "Baggins"
+}
+
 
 struct ContentView: View {
+    @State private var user = User()
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Tu nombre es \(user.firstName) \(user.lastName).")
+
+            TextField("Nombre", text: $user.firstName)
+            TextField("Apellido", text: $user.lastName)
         }
-        .padding()
     }
 }
 
