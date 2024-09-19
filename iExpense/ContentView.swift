baaa99443fc81822877184c020cd85999db9cbdc
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-09-18 18:28:39 -0600

    Archiving Swift objects with Codable

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -8,24 +8,26 @@
 import SwiftUI
 
 
-//struct ContentView: View {
-//    
-//    @State private var tapCount = UserDefaults.standard.integer(forKey: "Tap")
-//
-//    var body: some View {
-//        Button("Tap count: \(tapCount)") {
-//            tapCount += 1
-//            UserDefaults.standard.set(tapCount, forKey: "Tap")
-//        }
-//    }
-//}
+struct User: Codable {
+    let firstName: String
+    let lastName: String
+}
+
+
 struct ContentView: View {
+    @State private var user = User(firstName: "Taylor", lastName: "Swift")
+
     @AppStorage("tapCount") private var tapCount = 0
 
     var body: some View {
-        Button("Tap count: \(tapCount)") {
-            tapCount += 1
+        Button("Guardar usuario") {
+            let encoder = JSONEncoder()
+
+            if let data = try? encoder.encode(user) {
+                UserDefaults.standard.set(data, forKey: "UserData")
+            }
         }
+
     }
 }
 
