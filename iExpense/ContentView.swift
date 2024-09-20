0fe2c9f9c352524b16c250e3d3dcd30442ef9be0
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-09-19 18:35:22 -0600

    Making changes permanent with UserDefaults

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,26 +7,38 @@
 
 import SwiftUI
 
-struct ExpenseItem: Identifiable {
-    let id = UUID()
+struct ExpenseItem: Identifiable, Codable {
+    var id = UUID()
     let name: String
     let type: String
     let amount: Double
 }
 
-
 @Observable
 class Expenses {
-    var items = [ExpenseItem]()
+    var items = [ExpenseItem](){
+        didSet{
+            if let encoded = try? JSONEncoder().encode(items){
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: "Items") {
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
+                items = decodedItems
+                return
+            }
+        }
+        items = []
+    }
 }
 
 
 struct ContentView: View {
     @State private var expenses = Expenses()
-
     @State private var showingAddExpense = false
-
-    
+ 
     var body: some View {
         NavigationStack {
             List {
